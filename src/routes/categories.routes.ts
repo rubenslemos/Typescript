38e314651e11c13989a8c8102e12ed097fe20310
Rubens <rubenslemos@gmail.com>
2022-04-12 15:07:42 -0300

    Antes de aplicar LSP

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+
+import { Category } from '../model/Category';
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
+import {CreateCategoryService} from '../services/CreateCategoryService'
+const categoriesRoutes = Router()
+const categoriesRepository = new CategoriesRepository()
+categoriesRoutes.post('/', (request, response) => {
+  const { name, description } = request.body
+  
+  const createCategoryService = new CreateCategoryService(categoriesRepository)
+
+  createCategoryService.execute({name, description})
+  return response.status(201).send()
+
+})
+
+categoriesRoutes.get('/', (request, response) => {
+  const listAll = categoriesRepository.list()
+  return response.status(201).json({categorias: listAll})
+})
+export {categoriesRoutes}
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,8 @@
 import express from "express"
+import {categoriesRoutes} from "./routes/categories.routes"
+
 const app = express()
-app.get("/", (request, response) => response.json({mensagem: "Hello World!"}))
+app.use(express.json())
+
+app.use('/categories',categoriesRoutes)
 app.listen(3333, ()=> console.log("Running..."))
